1ec30009279912ecb780032add202bd638571e0c
Swashcap <swashcap@gmail.com>
2014-06-21 13:33:43 -0700

    Write styles build task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', []);
+gulp.task('styles', function () {
+  return gulp.src('assets/css/scss/main.scss')
+    .pipe(sass({
+      style: 'compressed',
+      precision: 10
+    }))
+    .pipe(autoprefixer('last 2 version', 'ie 9'))
+    .pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('default', ['styles']);
